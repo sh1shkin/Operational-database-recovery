b72ee86a8acdf8fdb589fa38077912c4c5b09ac1
Maksim <maksim_shishkin_04@inbox.ru>
2025-07-13 14:55:29 +0300

    develop

--- src/MyForm.cpp
+++ src/MyForm.cpp
@@ -1,10 +1,11 @@
 #include "MyForm.h"
 
 #include <Windows.h>
-
+#include <iostream>
 using namespace src;
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    setlocale(LC_ALL, "ru");
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
     Application::Run(gcnew MyForm);
